13c7dcff3b9513bf6175f4a1c7b1739fa301efdc
truptisatsangi <truptisatsangi18@gmail.com>
2025-04-13 12:45:43 +0530

    feat: add Monitoring script

---
+++ script/Monitoring.ts
@@ -0,0 +1,31 @@
+import { ethers } from "ethers";
+require("dotenv").config();
+import tokenAddress from "./Inputs/tokenAddress.json";
+import addressList from "./Inputs/addressList.json";
+
+const provider = new ethers.WebSocketProvider(
+  process.env.ALCHEMY_API_KEY as string
+);
+const token = tokenAddress.tokenAddress;
+const tokenABI = [
+  "event Transfer(address indexed from, address indexed to, uint256 value)",
+];
+
+const monitorAddresses = new Set(addressList.addressess);
+const tokenContract = new ethers.Contract(token, tokenABI, provider);
+
+console.log(`Monitoring ${token}`)
+
+tokenContract.on("Transfer", (from, to, value, event) => {
+  if (monitorAddresses.has(from) || monitorAddresses.has(to)) {
+    const amount = ethers.formatUnits(value, 6); // Adjust decimals as needed
+    const timestamp = new Date().toLocaleString();
+
+    console.log(`\nToken Transfer Detected at ${timestamp}`);
+    console.log(`From   : ${from}`);
+    console.log(`To     : ${to}`);
+    console.log(`Amount : ${amount}`);
+    console.log(`TxHash : ${event.log.transactionHash}`);
+    console.log("\nListening........\n")
+  }
+});
