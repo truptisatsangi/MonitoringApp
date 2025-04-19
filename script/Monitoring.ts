8e98f1f2bba767f4cb3c393fe905c48f04d97e11
truptisatsangi <truptisatsangi18@gmail.com>
2025-04-19 19:36:07 +0530

    feat: modify create to ensure to address must be a contract

--- script/Monitoring.ts
+++ script/Monitoring.ts
@@ -14,18 +14,30 @@ const tokenABI = [
 const monitorAddresses = new Set(addressList.addressess);
 const tokenContract = new ethers.Contract(token, tokenABI, provider);
 
-console.log(`Monitoring ${token}`)
+console.log(`Monitoring ${token}`);
 
-tokenContract.on("Transfer", (from, to, value, event) => {
+tokenContract.on("Transfer", async (from, to, value, event) => {
   if (monitorAddresses.has(from) || monitorAddresses.has(to)) {
     const amount = ethers.formatUnits(value, 6); // Adjust decimals as needed
-    const timestamp = new Date().toLocaleString();
 
-    console.log(`\nToken Transfer Detected at ${timestamp}`);
-    console.log(`From   : ${from}`);
-    console.log(`To     : ${to}`);
-    console.log(`Amount : ${amount}`);
-    console.log(`TxHash : ${event.log.transactionHash}`);
-    console.log("\nListening........\n")
+    const isToContract = await isContract(to);
+    if (value > ethers.parseUnits("100", 6) && !isToContract) {
+      const timestamp = new Date().toLocaleString();
+
+      console.log(`\nToken Transfer Detected at ${timestamp}`);
+      console.log(`From   : ${from}`);
+      console.log(`To     : ${to}`);
+      console.log(`Amount : ${amount}`);
+      console.log(`TxHash : ${event.log.transactionHash}`);
+      console.log("\nListening........\n");
+    }
   }
 });
+
+async function isContract(to: string) {
+  const code = await provider.getCode(to);
+  if (code.length == 2) {
+    return false;
+  }
+  return true;
+}
